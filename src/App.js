25227c676402bb8228c008327a624451998d0a38
gnanasai <gnanasai5111@gmail.com>
2021-12-23 17:17:21 +0530

    deploy

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import useBookSearch from "./useBookSearch";
+import {useState,useRef, useCallback} from 'react';
+function App() {
+  const [query,setQuery]=useState('');
+  const [pageNumber,setPageNumber]=useState(1);
+  const observer=useRef();
+  const {books,hasMore,loading,error}=useBookSearch(query,pageNumber);
+  const lastBookElementRef=useCallback(node=>{
+    if(loading) return
+    if(observer.current) observer.current.disconnect()
+    observer.current=new IntersectionObserver(entries=>{
+      if(entries[0].isIntersecting && hasMore){
+        setPageNumber(prevPageNumber=>prevPageNumber+1)
+      }
+
+    })
+    if(node) observer.current.observe(node)
+    console.log(node);
+  },[loading,hasMore]);
+
+  const handleSearch=(e)=>{
+    setQuery(e.target.value);
+    setPageNumber(1);
+  }
+ 
+
+
+  
+  return (
+   <>
+     <input type='text' value={query} onChange={handleSearch}></input>
+     {books.map((book,index)=>{
+       if(books.length===index+1){
+         return <div ref={lastBookElementRef} key={book} >{book}</div>
+       }
+       return <div  key={book} >{book}</div>
+     })}
+     <div>{loading && 'Loading...'}</div>
+     <div>{error && 'Error'}</div>
+   </>
+  );
+}
+
+export default App;
